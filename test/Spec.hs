4267782d8d284b456ecf273d9a6b768b80b6a1f8
Unknown <christopher.nyberg@gmail.com>
2017-02-04 15:07:44 -0800

    My first test suite in Haskell
    Huzzah!

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+
+import Test.Hspec
+
+import Lib as L
+
+main :: IO () 
+main = hspec $ do
+    describe "Addition" $ do
+        it "1 + 1 is greater than 1" $
+            (1 + 1) > 1 `shouldBe` True
+        it "2 + 2 is equal to 4" $
+            2 + 2 `shouldBe` 4
+    describe "Lib file" $ do
+        it "22 divided by 5 is 4 remainder 2" $
+            L.dividedBy 22 5 `shouldBe` (4, 2)
