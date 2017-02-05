01cd9087eb52e3caaf80de6b6d805652d82ca272
Unknown <christopher.nyberg@gmail.com>
2017-02-04 16:28:20 -0800

    My first QuickCheck
    From this moment forward, everything will be different.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,6 @@
 
 import Test.Hspec
+import Test.QuickCheck
 
 import Lib as L
 
@@ -7,9 +8,11 @@ main :: IO ()
 main = hspec $ do
     describe "Addition" $ do
         it "1 + 1 is greater than 1" $
-            (1 + 1) > 1 `shouldBe` True
+            (1 + 1 :: Int) > 1 `shouldBe` True
         it "2 + 2 is equal to 4" $
-            2 + 2 `shouldBe` 4
+            (2 + 2 :: Int) `shouldBe` 4
+        it "x + 1 is always greater than x" $
+            property $ \x -> x + 1 > (x :: Int)
     describe "Lib file" $ do
         it "22 divided by 5 is 4 remainder 2" $
-            L.dividedBy 22 5 `shouldBe` (4, 2)
+            L.dividedBy (22 :: Int) (5 :: Int) `shouldBe` (4, 2)
